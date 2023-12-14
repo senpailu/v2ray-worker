75f333a477e8ec20b403b62bafb0bb745576c957
SonzaiEkkusu <abhiedthea@gmail.com>
2023-12-14 19:09:46 +0700

    X

--- src/panel.ts
+++ src/panel.ts
@@ -76,8 +76,7 @@ export async function GetPanel(request: Request, env: Env): Promise<Response> {
         <div class="p-1 bg-primary text-white">
           <div class="text-nowrap fs-4 fw-bold text-center">V2RAY Worker - Control Panel</div>
           <div class="text-nowrap fs-6 text-center">
-            Version 2.2 by
-            <a href="https://twitter.com/vahidfarid" target="_blank" class="text-white">Vahid Farid</a>
+            Version 2.2
           </div>
         </div>
         ${htmlMessage}
@@ -237,8 +236,7 @@ export async function GetPanel(request: Request, env: Env): Promise<Response> {
           <div class="p-1 bg-primary text-white">
             <div class="text-nowrap fs-4 fw-bold text-center">V2RAY Worker - Control Panel</div>
             <div class="text-nowrap fs-6 text-center">
-              Version 2.2 by
-              <a href="https://twitter.com/vahidfarid" target="_blank" class="text-white">Vahid Farid</a>
+              Version 2.2
             </div>
           </div>
           <div class="px-5 py-2 bg-light">
